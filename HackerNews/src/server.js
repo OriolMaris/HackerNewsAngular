a570ae403a54e091735491ed754d71ec945c85d6
oriol.maristany <oriol.maristany@est.fib.upc.edu>
2020-05-28 20:23:07 +0200

    Fix

--- HackerNews/src/server.js
+++ HackerNews/src/server.js
@@ -3,9 +3,12 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-blog'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-blog/index.html'));
+app.use(express.static('./dist/package.json>'));
+
+
+app.get('/*', function(req, res) {
+  res.sendFile('index.html', {root: 'dist/package.json>/'}
+);
 });
 
 app.listen(process.env.PORT || 8080);
